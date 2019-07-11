007c48625c1f3e3a78746e3fda07516d93311fb3
xinpeilin <741738362@qq.com>
2019-07-11 17:56:27 +0800

    18点

--- TestDemo/src/com/ss/Day01.java
+++ TestDemo/src/com/ss/Day01.java
@@ -1,31 +1,51 @@
 package com.ss;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Day01 {
-	
-	// 给定一个数组和一个数，求解相加等于这个数的数组下标。 
-	
-		public static void main(String[] args) {
-			
-			int [] nums = {2, 7, 11, 15};
-			int target = 9;
-			
-			System.out.println(Arrays.toString(twoSum(nums,target)));
+
+	// 给定一个数组和一个数，求解相加等于这个数的数组下标。
+
+	public static void main(String[] args) {
+
+		int[] nums = { 2, 7, 11, 15 };
+		int target = 9;
+
+		System.out.println(Arrays.toString(twoSum1(nums, target)));
+		System.out.println(Arrays.toString(twoSum2(nums, target)));
+	}
+
+	public static int[] twoSum1(int[] nums, int target) {
+
+		int[] res = new int[2];
+		for (int i = 0; i < nums.length - 1; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					res[0] = i;
+					res[1] = j;
+				}
+			}
 		}
-		
-		
-		public static int[] twoSum(int[] nums, int target) {
-			
-	        int[] res = new int[2];
-	        for(int i = 0; i < nums.length-1; i++) {
-	            for(int j = i+1; j < nums.length; j++) {
-	                if(nums[i] + nums[j] == target) {
-	                    res[0] = i;
-	                    res[1] = j;
-	                }
-	            }
-	        }
-	        return res;
-	    }
+		return res;
+	}
+
+	public static int[] twoSum2(int[] nums, int target) {
+
+		int[] res = new int[2];
+
+		HashMap<Integer, Integer> map = new HashMap<>();
+
+		for (int i = 0; i < nums.length; i++) {
+			map.put(nums[i], i);
+		}
+
+		for (int k = 0; k < nums.length; k++) {
+			if (map.get(target - nums[k]) != null && map.get(target - nums[k]) != k) {
+				res[0] = k;
+				res[1] = map.get(target - nums[k]);
+			}
+		}
+		return res;
+	}
 }
--- TestDemo/src/com/ss/Day02.java
+++ TestDemo/src/com/ss/Day02.java
@@ -1,10 +1,46 @@
 package com.ss;
 
-import java.util.Arrays;
 
 public class Day02 {
 
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode tail = null;
+        //余数
+        int remainder = 0;
+        
+        while (l1 != null || l2 != null || remainder > 0) {
+            int a = 0, b = 0;
+            if (l1 != null) {
+                a = l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                b = l2.val;
+                l2 = l2.next;
+            }
+            //记录余数.
+            int item = remainder + a + b;
+            remainder = item >= 10 ? 1 : 0;
+            ListNode current = new ListNode(item % 10);
+            if (tail == null) {
+                tail = current;
+                head = current;
+            } else {
+                tail.next = current;
+                tail = tail.next;
+            }
+        }
+        return head;
+    }
 	
+	
+	
+	  public class ListNode {
+		  int val;
+		  ListNode next;
+		  ListNode(int x) { val = x; }
+	  }
 }
 
 
